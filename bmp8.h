64728aa571fbffaea6f0b0bfe21d5426e99e5adc
antonin <antonin.autret@efrei.net>
2025-03-20 18:06:35 +0100

    creation of the t_bmp8 structure

--- bmp8.h
+++ bmp8.h
@@ -2,7 +2,13 @@
 #define BMP8_H
 
 typedef struct{
-
+    unsigned char header[54];
+    unsigned char colorTable[1024];
+    unsigned char * data;
+    unsigned int width;
+    unsigned int height;
+    unsigned int colorDepth;
+    unsigned int dataSize;
 
 }t_bmp8;
 
