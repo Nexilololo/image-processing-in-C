426f6b6687e6a010cdb7b7757c8af456dead18a7
antonin <antonin.autret@efrei.net>
2025-03-20 18:08:41 +0100

    creating the prototypes for the bmp8.c file's functions

--- bmp8.h
+++ bmp8.h
@@ -12,4 +12,8 @@ typedef struct{
 
 }t_bmp8;
 
+t_bmp8 * bmp8_loadImage(const char * filename);
+void bmp8_saveImage(const char * filename, t_bmp8 * img);
+void bmp8_free(t_bmp8 * img);
+void bmp8_printInfo(t_bmp8 * img);
 #endif //BMP8_H
