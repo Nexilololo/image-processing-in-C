ec1f4e165d7cec14469996f458b8a077e1fdffea
Charles Guégan <nexilog1706@gmail.com>
2025-05-23 09:42:11 +0200

    Revert "Test"
    This reverts commit 69ba8ff15894904de397762583908823471f637b.

--- bmp8.h
+++ bmp8.h
@@ -2,25 +2,36 @@
 #define BMP8_H
 
 #include <stdio.h>
+#include <stdint.h> // For uintN_t types
 
-typedef struct{
+typedef struct {
     unsigned char header[54];
     unsigned char colorTable[1024];
-    unsigned char * data;
+    unsigned char *data;
     unsigned int width;
     unsigned int height;
     unsigned int colorDepth;
-    unsigned int dataSize;
-}t_bmp8;
+    unsigned int dataSize; // Size of the pixel data array
+} t_bmp8;
 
-t_bmp8 * bmp8_loadImage(const char * filename);
-void bmp8_saveImage(const char * filename, t_bmp8 * img);
-void bmp8_free(t_bmp8 * img);
-void bmp8_printInfo(t_bmp8 * img);
+// Reading and Writing
+t_bmp8 *bmp8_loadImage(const char *filename);
+void bmp8_saveImage(const char *filename, t_bmp8 *img);
+void bmp8_free(t_bmp8 *img);
+void bmp8_printInfo(t_bmp8 *img);
 
-void bmp8_negative(t_bmp8 * img);
-void bmp8_brightness(t_bmp8 * img, int value);
-void bmp8_threshold(t_bmp8 * img, int threshold);
-void bmp8_applyFilter(t_bmp8 * img, double * kernel, int kernelSize);
+// Image Processing Functions
+void bmp8_negative(t_bmp8 *img);
+void bmp8_brightness(t_bmp8 *img, int value);
+void bmp8_threshold(t_bmp8 *img, int threshold_val);
 
-#endif //BMP8_H
+// Image Filtering
+// The kernel is passed as an argument, its definition will be in main.c
+void bmp8_applyFilter(t_bmp8 *img, float kernel[3][3], int kernelSize);
+
+// Histogram Equalization
+unsigned int *bmp8_computeHistogram(t_bmp8 *img);
+unsigned int *bmp8_computeCDF(unsigned int *hist);
+void bmp8_equalize(t_bmp8 *img);
+
+#endif // BMP8_H
